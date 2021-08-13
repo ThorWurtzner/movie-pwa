a0495a3ea317d13662afcb8af02e455138969197
Thor Würtzner <6273158@rts-365.dk>
2021-08-13 08:53:23 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -9,3 +9,14 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/sw.js").then((registration) => {
+            console.log("SW sucessfully registered", registration);
+        }, (error) => {
+            console.log("SW failed to register", error);
+        })
+    })
+}
